6d34729d0e9cfd301546c9f27e3a4f5599100989
KONOHA-GANGSTA <piligrim507@gmail.com>
2022-05-13 21:56:28 +0300

    Задание1 (без рисунка)

---
+++ js/init.js
@@ -0,0 +1,47 @@
+$(function(){
+	
+	
+	$('.slider1').mobilyslider({
+		transition: 'fade',
+		animationSpeed: 800,
+		bullets: true,
+		arrowsHide: false
+	});
+
+	$('.slider2').mobilyslider({
+		transition: 'fade',
+		animationSpeed: 800,
+		bullets: true,
+		arrowsHide: false
+	});
+
+	$('.slider3').mobilyslider({
+		transition: 'fade',
+		animationSpeed: 800,
+		bullets: true,
+		arrowsHide: false
+	});
+
+	$('.slider4').mobilyslider({
+		transition: 'fade',
+		animationSpeed: 800,
+		bullets: true,
+		arrowsHide: false
+	});
+
+	$('.slider5').mobilyslider({
+		transition: 'fade',
+		animationSpeed: 800,
+		bullets: true,
+		arrowsHide: false
+	});
+
+	$('.slider6').mobilyslider({
+		transition: 'fade',
+		animationSpeed: 800,
+		bullets: true,
+		arrowsHide: false
+	});
+	
+	
+});
